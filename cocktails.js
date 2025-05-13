f3ac60c87fefcf36dce14982c6c55e8da4f692c1
ArtemBartender <Bilenckotema10@gmail.com>
2025-05-13 11:44:01 +0200

    Add files via upload

--- cocktails.js
+++ cocktails.js
@@ -1,4 +1,3 @@
-
 const cocktails = [
   {
     name: { pl: "Blue Lagoon", en: "Blue Lagoon" },
@@ -119,5 +118,78 @@ const cocktails = [
     name: { pl: "Pineapple Mint Splash", en: "Pineapple Mint Splash" },
     ingredients: { pl: ["Sok ananasowy", "Syrop miętowy", "Sok z cytryny", "Lód"], en: ["Pineapple juice", "Mint syrup", "Lemon juice", "Ice"] },
     category: "non-alcoholic"
+  }
+,
+  {
+    name: { pl: "Mimosa", en: "Mimosa" },
+    ingredients: {
+      pl: ["Sok Pomarańczowy", "Wermut"],
+      en: ["Orange juice", "Vermouth"]
+    },
+    category: "liqueur"
+  },
+  {
+    name: { pl: "Hurricane", en: "Hurricane" },
+    ingredients: {
+      pl: ["Rum", "Sok z owoców cytrusowych", "Grenadyna"],
+      en: ["Rum", "Citrus juice", "Grenadine"]
+    },
+    category: "rum"
+  },
+  {
+    name: { pl: "Irishman", en: "Irishman" },
+    ingredients: {
+      pl: ["Irish Cream", "Whiskey"],
+      en: ["Irish Cream", "Whiskey"]
+    },
+    category: "liqueur"
+  },
+  {
+    name: { pl: "Gold Rush", en: "Gold Rush" },
+    ingredients: {
+      pl: ["Whiskey", "Likier miodowy", "Sok z cytryny"],
+      en: ["Whiskey", "Honey liqueur", "Lemon juice"]
+    },
+    category: "whiskey"
+  },
+  {
+    name: { pl: "Old Manhattan", en: "Old Manhattan" },
+    ingredients: {
+      pl: ["Bourbon", "Wermut", "Angostura"],
+      en: ["Bourbon", "Vermouth", "Angostura"]
+    },
+    category: "whiskey"
+  },
+  {
+    name: { pl: "Blue Orange Whiskey Sour", en: "Blue Orange Whiskey Sour" },
+    ingredients: {
+      pl: ["Whiskey", "Syrop blue curacao", "Syrop cukrowy", "Sok z cytryny"],
+      en: ["Whiskey", "Blue curacao syrup", "Sugar syrup", "Lemon juice"]
+    },
+    category: "whiskey"
+  },
+  {
+    name: { pl: "The Curious Jackalope", en: "The Curious Jackalope" },
+    ingredients: {
+      pl: ["Gin", "Sok z limonki", "Sok z grejpfruta", "Syrop cukrowy"],
+      en: ["Gin", "Lime juice", "Grapefruit juice", "Sugar syrup"]
+    },
+    category: "gin"
+  },
+  {
+    name: { pl: "Old Fashioned", en: "Old Fashioned" },
+    ingredients: {
+      pl: ["Bourbon", "Cukier", "Angostura"],
+      en: ["Bourbon", "Sugar", "Angostura"]
+    },
+    category: "whiskey"
+  },
+  {
+    name: { pl: "Szarlotka", en: "Apple Pie" },
+    ingredients: {
+      pl: ["Wódka", "Sok jabłkowy", "Syrop waniliowy", "Sok z cytryny"],
+      en: ["Vodka", "Apple juice", "Vanilla syrup", "Lemon juice"]
+    },
+    category: "vodka"
   }
 ];
