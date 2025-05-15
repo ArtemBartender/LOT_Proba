0c261b04bd941af856d83cf0ab016c5ca12dd2df
ArtemBartender <Bilenckotema10@gmail.com>
2025-05-15 16:03:51 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'lot-n-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  // './styles.css', './app.js', './icons/icon-192.png', …
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
+  );
+});
